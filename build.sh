6b5310a8f2321b2f61f29af077400b3d64e9b0b8
James Ge <james.ge@gmail.com>
2023-12-30 13:57:30 +0800

    MNT

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build . -t registry.cn-shanghai.aliyuncs.com/trade/cats_tradeserver_py:0.1
+docker build . -t registry.cn-shanghai.aliyuncs.com/trade/cats_tradeserver_py:0.2
+docker push registry.cn-shanghai.aliyuncs.com/trade/cats_tradeserver_py:0.2
